42a8a2b7201512edb7bd509fa86e86ea25292d6b
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2022-08-29 20:53:27 +0600

    car body

--- src/script.js
+++ src/script.js
@@ -1,47 +1,125 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import gsap from 'gsap';
+import * as dat from 'dat.gui';
+
+// Debeg
+const gui  = new dat.GUI();
+
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+    width: window.innerWidth - 200,
+    height: window.innerHeight 
+};
 
 // Cursor
 const cursor = {
     x: 0,
     y: 0
-}
+};
 
 window.addEventListener('mousemove', (event) =>
 {
     cursor.x = event.clientX / sizes.width - 0.5
     cursor.y = - (event.clientY / sizes.height - 0.5)
-})
+});
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
 
 // Object
 const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    new THREE.PlaneGeometry(60, 60),
+    new THREE.MeshBasicMaterial({color: 0xCFD2CF, side: THREE.DoubleSide} )
 )
 scene.add(mesh)
 
+mesh.rotation.x = 1.57;
+mesh.position.y = -1.06;
+
+gui.add(mesh.position,'y',-3,3,0.01);
+
+function createWheels() {
+  const geometry = new THREE.CylinderGeometry(7, 7, 35, 50);
+  const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
+  const wheel = new THREE.Mesh(geometry, material);
+  return wheel;
+}
+
+
+function createCar() {
+  const car = new THREE.Group();
+  
+  const backWheel = createWheels();
+  backWheel.position.y = 6;
+  backWheel.position.x = -18;
+  backWheel.rotation.x = 1.57;
+  car.add(backWheel);
+  
+  const frontWheel = createWheels();
+  frontWheel.position.y = 6;  
+  frontWheel.position.x = 18;
+  frontWheel.rotation.x = 1.57;
+  car.add(frontWheel);
+
+
+  const main = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(60, 15, 30),
+    new THREE.MeshLambertMaterial({ color: 0x78b14b })
+  );
+  main.position.y = 12;
+  car.add(main);
+
+  const cabin = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(33, 12, 24),
+    new THREE.MeshLambertMaterial({ color: 0xffffff })
+  );
+  cabin.position.x = -6;
+  cabin.position.y = 25.5;
+  car.add(cabin);
+
+  return car;
+}
+
+const car = createCar();
+scene.add(car);
+
+
+//light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(200, 500, 300);
+scene.add(directionalLight); 
+
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
-camera.position.z = 3
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const aspectRatio = sizes.width / sizes.height;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth/ aspectRatio;
+const camera = new THREE.OrthographicCamera(
+    cameraWidth / -2, // left
+     cameraWidth / 2, // right
+     cameraHeight / 2, // top
+     cameraHeight / - 2, // bottom
+     0, 
+     10000)
+camera.position.set(200, 200, 200)
+camera.up.set(0,1,0);
+camera.lookAt(0, 0, 0);
 scene.add(camera)
+
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -54,8 +132,12 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
+renderer.render(scene, camera)
+camera.lookAt(0, 10, 0);
+
 // Animate
 const clock = new THREE.Clock()
+
 
 const tick = () =>
 {
